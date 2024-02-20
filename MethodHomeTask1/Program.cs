830b077992c69d6e9c544f4a75d97ac19714e4e4
AykhanTa <aykhanft@code.edu.az>
2024-02-21 00:53:58 +0400

    Add project files.

---
+++ MethodHomeTask1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MethodHomeTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FirstTask(10,20));
+            
+        }
+
+        static int FirstTask(int a,int b)
+        {
+            return a / 2;
+        }
+    }
+}
