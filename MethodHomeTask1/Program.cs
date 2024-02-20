300fe8493dc6f32f2cf7f919421751b5d741a9da
AykhanTa <aykhanft@code.edu.az>
2024-02-21 01:11:57 +0400

    FourthTask

--- MethodHomeTask1/Program.cs
+++ MethodHomeTask1/Program.cs
@@ -5,7 +5,8 @@
         static void Main(string[] args)
         {
             //Console.WriteLine(FirstTask(10,20));
-            Console.WriteLine(ThirdTask(5,20));
+            //Console.WriteLine(ThirdTask(5,20));
+            Console.WriteLine(FourthTask(15));
 
         }
 
@@ -32,6 +33,28 @@
             {
                 return 0;
             }
+        }
+
+        static string FourthTask(int n)
+        {
+            bool sade = true;
+            if (n == 2)
+            {
+                return "ne sade ne murekkeb";
+            }
+            else
+            {
+                for (int i = 2; i < n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        sade = false;
+                        break;
+                    }
+                }
+            }
+            return sade ? "sadedir" : "murekkebdir";
+
         }
     }
 }
