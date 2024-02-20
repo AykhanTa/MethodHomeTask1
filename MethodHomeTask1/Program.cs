f5611091c16743d6425033c4ddf72cde92b6221d
AykhanTa <aykhanft@code.edu.az>
2024-02-21 01:02:00 +0400

    task3

--- MethodHomeTask1/Program.cs
+++ MethodHomeTask1/Program.cs
@@ -4,13 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(FirstTask(10,20));
-            
+            //Console.WriteLine(FirstTask(10,20));
+            Console.WriteLine(ThirdTask(5,20));
+
         }
 
         static int FirstTask(int a,int b)
         {
             return a / 2;
+        }
+
+        static int ThirdTask(int n, int m)
+        {
+            int count = 0;
+            if (n < m)
+            {
+                for (int i = n; i < m; i++)
+                {
+                    if (i % 2 == 1)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
 }
